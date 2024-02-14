a6dbc3222efd25e9623fa175cc8a9040a8de791d
Sleightttt <brennancota@yahoo.com>
2024-02-14 15:10:03 -0800

    added backend boiler

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const routes = require("./routes");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use(routes);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log("Server is running on port 5000");
 });
